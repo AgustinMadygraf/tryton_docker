232ced2311b4fe78cbd5e2d4babd9405dc515bc0
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2024-09-20 13:40:32 -0300

    Refactor run.py to remove unused code

--- run.py
+++ run.py
@@ -5,10 +5,6 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 
 from src.app import main
-from src.models.update_repo import RepoUpdater
 
 if __name__ == '__main__':
-    repo_path = 'C:\AppServ\www\AnalizadorDeProyecto'
-    updater = RepoUpdater(repo_path)
-    updater.run()    
     main()
