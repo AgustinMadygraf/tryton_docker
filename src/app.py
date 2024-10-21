3ea2a9600535a2a0522310a943ff5089e541803c
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2024-10-21 20:17:08 -0300

    Refactor app.py to update ngrok port and reorder code

--- src/app.py
+++ src/app.py
@@ -27,7 +27,7 @@ def manage_containers(container_manager, containers):
 
 def start_ngrok():
     # Inicia un túnel HTTP en el puerto 5000
-    http_tunnel = ngrok.connect(5000)
+    http_tunnel = ngrok.connect(8000)
     print(f" * ngrok tunnel \"{http_tunnel.public_url}\" -> \"http://localhost:8000\"")
 
 def main():
@@ -35,6 +35,6 @@ def main():
     container_manager = ContainerManager()
     docker_manager.initialize_docker()
     manage_containers(container_manager, CONTAINERS)
-    countdown(10, "Finalizando")
-    logger.info("Tryton está listo para usarse. Abre tu navegador en http://localhost:8000 e inicia sesión con tus credenciales.")
     start_ngrok()
+    logger.info("Tryton está listo para usarse. Abre tu navegador en http://localhost:8000 e inicia sesión con tus credenciales.")
+    countdown(10, "Finalizando")
