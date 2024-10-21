ca2fd5d561f2d42120cc98882117046398698697
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2024-10-21 20:36:38 -0300

    Refactor app.py to update ngrok configuration and handle errors

--- src/app.py
+++ src/app.py
@@ -3,8 +3,9 @@ from .docker_manager import DockerManager
 from .container_manager import ContainerManager
 from .utils.command_utils import countdown
 from src.logs.config_logger import LoggerConfigurator
-from pyngrok import ngrok
-
+import atexit
+from pyngrok import ngrok, conf
+from pyngrok.exception import PyngrokNgrokError
 
 logger_configurator = LoggerConfigurator()
 logger = logger_configurator.get_logger()
@@ -26,9 +27,24 @@ def manage_containers(container_manager, containers):
                 container_manager.setup_tryton_database()
 
 def start_ngrok():
-    # Inicia un túnel HTTP en el puerto 5000
-    http_tunnel = ngrok.connect(8000)
-    print(f" * ngrok tunnel \"{http_tunnel.public_url}\" -> \"http://localhost:8000\"")
+    try:
+        # Configurar ngrok
+        conf.get_default().region = "us"  # Cambia la región si es necesario
+
+        # Inicia un túnel HTTP en el puerto 8000
+        http_tunnel_8000 = ngrok.connect(8000)
+        print(f" * ngrok tunnel \"{http_tunnel_8000.public_url}\" -> \"http://localhost:8000\"")
+
+
+        # Registrar la función de cierre para asegurarse de que los túneles se cierren correctamente
+        atexit.register(ngrok.disconnect, http_tunnel_8000.public_url)
+
+    except PyngrokNgrokError as e:
+        print(f"Error al iniciar ngrok: {e}")
+        if "ERR_NGROK_3200" in str(e):
+            print("Tunnel not found. Please check your ngrok configuration and try again.")
+    except Exception as e:
+        print(f"Error inesperado: {e}")
 
 def main():
     docker_manager = DockerManager()
